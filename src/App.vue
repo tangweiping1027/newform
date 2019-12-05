0ef71aeebcd916b68b73d94f1851556296804307
weipingtang <weipingtang@shangshibang.com>
2019-12-05 09:18:03 +0800

    test update

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <el-tabs v-model="activeName" type="card" @tab-click="handleClick">
       <el-tab-pane v-for="item in tabList" :key="item.id" :label="item" :name="item">
         <router-view />
+        <!-- 什么鬼 -->
       </el-tab-pane>
     </el-tabs>
   </div>
