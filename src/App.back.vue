8a87e1c138e0c29a65ef829f6c4566d853405c28
weipingtang <weipingtang@shangshibang.com>
2019-12-09 16:45:58 +0800

    master 提交

---
+++ src/App.back.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <el-tabs v-model="activeName" type="card" @tab-click="handleClick">
+      <el-tab-pane v-for="item in tabList" :key="item.id" :label="item" :name="item">
+        <router-view />
+      </el-tab-pane>
+    </el-tabs>
+  </div>
+</template>
+
+<script>
+import $ from "jquery";
+export default {
+  name: "app",
+  data () {
+    return {
+      activeName: "",
+      tabList: []
+    }
+  },
+  created () {
+    // eslint-disable-next-line no-console
+    console.log()
+    this.tabList = this.$router.options.routes.map(item => item.name)
+    this.activeName = this.$route.name
+    $(window).on("resize", () => {
+      this.$eventBus.$emit('window-resize')
+    })
+  },
+  watch: {
+    "$route.name"(val) {
+      this.activeName = val
+    }
+  },
+  methods: {
+    handleClick (tab) {
+    // eslint-disable-next-line no-console
+      window.open(`${window.location.host}/${tab.name}`)
+      this.$router.push({
+        path: `/${tab.name}`
+      })
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 20px;
+}
+</style>
--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,22 @@
 <template>
-  <div id="app">
-    <el-tabs v-model="activeName" type="card" @tab-click="handleClick">
-      <el-tab-pane v-for="item in tabList" :key="item.id" :label="item" :name="item">
-        <router-view />
-        <!-- 什么鬼 -->
-      </el-tab-pane>
-    </el-tabs>
-  </div>
+ <div class='app'>
+   
+ </div>
 </template>
 
 <script>
-import $ from "jquery";
-export default {
-  name: "app",
-  data () {
-    return {
-      activeName: "",
-      tabList: []
-    }
-  },
-  created () {
-    // eslint-disable-next-line no-console
-    console.log()
-    this.tabList = this.$router.options.routes.map(item => item.name)
-    this.activeName = this.$route.name
-    $(window).on("resize", () => {
-      this.$eventBus.$emit('window-resize')
-    })
-  },
-  watch: {
-    "$route.name"(val) {
-      this.activeName = val
-    }
-  },
-  methods: {
-    handleClick (tab) {
-    // eslint-disable-next-line no-console
-      window.open(`${window.location.host}/${tab.name}`)
-      this.$router.push({
-        path: `/${tab.name}`
-      })
-    }
-  }
-};
+ export default {
+   data () {
+     return {
+
+     }
+   },
+ }
 </script>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 20px;
+<style lang='scss'>
+
+.app {
+background: #fff
 }
 </style>
